a7779bfccecf1248d3bf51b977a1142d5b47e4a6
thealokkushwaha <alokkushwaha1005@gmail.com>
2022-09-18 07:14:28 +0530

    Commit3

---
+++ Virtual Mall/Assets/playercontroller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+        transform.position += moveDirection;
+    }
+}
